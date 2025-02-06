1aeadc3dcb7d61b15e0671d4f0162fe7651a30ce
BugHunter-X <askzenai@gmail.com>
2025-02-06 16:00:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the `useEffect` hook in React Native with a cleanup function that throws an error. This can happen if the cleanup function tries to access a component's state or props after the component has unmounted.
